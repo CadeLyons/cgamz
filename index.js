24ac168448d8bd8a05dcdee301ade17154acdc9f
CadeLyons <CadeLyons1236@gmail.com>
2022-02-15 23:40:13 +0000

    added getDistance

--- index.js
+++ index.js
@@ -58,7 +58,15 @@
            */
           update(event) {
             // template method //
-          }
+          },
+          getDistance(pointA, pointB) {
+            const
+              distanceX = pointB.x - pointA.x,
+              distanceY = pointB.y - pointA.y,
+              distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            return distance;
+          },
+          
         };
       },
     },
